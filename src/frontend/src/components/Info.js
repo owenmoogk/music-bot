39eac4da336e66affc8bde89828fc4d279d72553
owenmoogk <owenmoogk@gmail.com>
2021-04-21 11:43:03 -0400

    changed the content on the info page

--- src/frontend/src/components/Info.js
+++ src/frontend/src/components/Info.js
@@ -16,13 +16,37 @@ export default function Info(props){
 	// methods
 	function joinInfo(){
 		return(
-			'Join page'
+			<Grid align='center' container spacing={1}>
+				<Grid item xs={12}>
+					<Typography component='h4' variant='h4'>
+						What is House Party?
+					</Typography>
+				</Grid>
+				<Grid item xs={12}>
+					<Typography component='p' variant='h6'>
+						House Party is an application that, using the Spotify API, allows a group to control music playback, with
+						functionality for voting to skip songs, hosting multiple rooms, and pausing and playing songs. The host can control
+						the permissions of the users on the 'Host Settings' page, so it will fit whatever needs you desire. Enjoy!
+					</Typography>
+				</Grid>
+			</Grid>
 		)
 	}
 
 	function createInfo(){
 		return(
-			'Create page'
+			<Grid align='center' container spacing={1}>
+				<Grid item xs={12}>
+					<Typography component='h4' variant='h4'>
+						Why this app?
+					</Typography>
+				</Grid>
+				<Grid item xs={12}>
+					<Typography component='p' variant='h6'>
+						I was bored during quarentine, and had just started with django. I had no idea how to integrate django with react, and learned an absolute butt-ton doing this. I used a tech with tim series as a guideline, and it went great :)
+					</Typography>
+				</Grid>
+			</Grid>
 		)
 	}
 
@@ -41,11 +65,6 @@ export default function Info(props){
 
 	return(
 		<Grid container spacing={1}>
-			<Grid item xs={12} align='center'>
-				<Typography component='h4' variant='h4'>
-					What is House Party?
-				</Typography>
-			</Grid>
 			<Grid item xs={12} align='center'>
 				<Typography component='h4' variant='h4'>
 					{ page === pages.JOIN ? joinInfo():createInfo()}
